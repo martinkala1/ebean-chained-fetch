18ca9da8a75dc154154033f1b13728a5e38f743a
Martin Kala <martin.kala@sentica.cz>
2025-06-16 13:21:11 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# ebean-chained-fetch
-Isolated case of chained fetch statement 
+# Chained fetch() calls on ebean query-bean
+
+This repo demonstrates the issue, where multiple calls of `fetch()` on a QueryBean does not behave as expected and only
+the first `fetch()` actually executes, the rest silently fails.
+
+### Test
+The `MainTest.kt` contains the tests to verify functionality. The created person has 2 pets and 2 cars, but when
+that person is retrieved from the database, only the first `fetch()` statement is effective, the others are ignored.
+
+### Expected Behavior
+After executing both of the tests, the console should print `Bobby Somedude has 2 cars and 2 pets.` 
+
+### Current Behavior
+In `personTest_carsWork`, the console prints `Bobby Somedude has 2 cars and 0 pets.`. 
+
+In `personTest_petsWork`, the console prints `Bobby Somedude has 0 cars and 2 pets.`. 
+
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 
 class MainTest {
     @Test
-    fun personTest() {
+    fun personTest_carsWork() {
         val bobby = Person(firstName = "Bobby", lastName = "Somedude")
         bobby.save()
         val kitty = Pet("Kotik", "Cat", bobby)
@@ -26,6 +26,36 @@ class MainTest {
             QPerson()
                 .fetch("cars", "*")
                 .fetch("pets", "*")
+                .where()
+                .lastName
+                .ieq("somedude")
+                .findOne()
+
+        assert(foundBobby != null)
+        println(foundBobby)
+    }
+
+    @Test
+    fun personTest_petsWork() {
+        val bobby = Person(firstName = "Bobby", lastName = "Somedude")
+        bobby.save()
+        val kitty = Pet("Kotik", "Cat", bobby)
+        val doggie = Pet("Baron", "Dog", bobby)
+        kitty.save()
+        doggie.save()
+        val skoda = Car("Skoda", 2003, bobby)
+        val tractor = Car("Zetor", 1984, bobby)
+        skoda.save()
+        tractor.save()
+
+        bobby.pets = listOf(kitty, doggie)
+        bobby.cars = listOf(skoda, tractor)
+        bobby.save()
+
+        val foundBobby =
+            QPerson()
+                .fetch("pets", "*")
+                .fetch("cars", "*")
                 .where()
                 .lastName
                 .ieq("somedude")
